a1440a6a1b3eaabc41bbfaa49d13148aa90b035c
ts3rules <toby.simmons@ymail.com>
2026-07-27 21:56:38 +0100

    Menus added
    copied menus from knoutout and changed slighly to work in ballistix

---
+++ Assets/Scripts/Ballistix 2 Scripts/BallistixmainMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class BallstixmainMenu : MonoBehaviour
+  
+ 
+{
+    public GameObject optionsMenu;
+    public GameObject mainMenuUI;
+    public GameObject choosePlayers;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Knockout Game");
+    }
+
+    public void options()
+    {
+        choosePlayers.SetActive(false);
+        mainMenuUI.SetActive(false);
+        optionsMenu.SetActive(true);
+
+    }
+
+    public void MainMenu()
+    {
+        choosePlayers.SetActive(false);
+        optionsMenu.SetActive(false);
+        mainMenuUI.SetActive(true);
+    }
+
+    public void PlayerCount()
+    {
+        optionsMenu.SetActive(false);
+        mainMenuUI.SetActive(false);
+        choosePlayers.SetActive(true);
+    }
+
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }   
+
+   
+
+
+}
+
